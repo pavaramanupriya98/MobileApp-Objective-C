5d56fe9002e792015e54e8ee4faa9ab3fd7e9fca
Pavara Manupriya <pavara@4axissolutions.com>
2021-09-12 15:52:18 +0530

    Login Function Created

--- MyFirstProject/ViewController.m
+++ MyFirstProject/ViewController.m
@@ -9,6 +9,13 @@
 
 @interface ViewController ()
 
+@property ( nonatomic, strong ) NSString *username;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property ( nonatomic, strong ) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
+
+
 @end
 
 @implementation ViewController
@@ -16,7 +23,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.username = @"Pavara";
+    self.password = @"securepw";
+    
+    self.passwordTextField.secureTextEntry=YES;
+    
 }
+- (IBAction)loggingWasPressed:(id)sender {
+    BOOL isUsersEqual =[self.username isEqualToString:[self.usernameTextField text]];
+    BOOL isPasswordsEqual = [self.password isEqualToString:[self.passwordTextField text]];
+    if(isUsersEqual && isPasswordsEqual){
+        NSLog(@"SUCCESS");
+        [self.notificationLabel setText: @"Congratulations you have logged in!"];
+        //NSNotification(@"Congratulations you have logged in!");
+    }
+    else{
+        NSLog(@"Failed");
+        [self.notificationLabel setText:@"Username of Password was incorrect"];
+        
+    }
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
+}
+
 
 
 @end
